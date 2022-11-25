d8e0a5a213ff4cabd729e761e531f595c183af1f
hegde-atri <iamatrihegde@outlook.com>
2022-11-25 11:26:59 +0000

    :sparkles: added SEO, assets and skeleton pages/components

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+	title: 'Atri Hegde',
+	description: 'Personal Portfolio website, with helpful posts and guides.',
+	defaultLanguage: 'en_GB',
+};
+
+export const OPEN_GRAPH = {
+	image: {
+		src: 'https://github.com/hegde-atri',
+		alt: 'Banner',
+	},
+	twitter: 'astrodotbuild',
+};
+
+export const KNOWN_LANGUAGES = {
+	English: 'en',
+};
+
+// For ALGOLIA search
+// export const ALGOLIA = {
+//   indexName: 'XXXXXXXXXX',
+//   apiKey: 'XXXXXXXXXX',
+// }
