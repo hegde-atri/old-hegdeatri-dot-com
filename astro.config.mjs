0f32d007f6021b3c8bcf7fb1a14029b38be07ea5
hegde-atri <iamatrihegde@outlook.com>
2023-01-14 21:50:02 +0000

    added tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://new.hegdeatri.com',
-  integrations: [
-    mdx(),
-    sitemap(),]
+  integrations: [mdx(), sitemap(), tailwind()]
 });
