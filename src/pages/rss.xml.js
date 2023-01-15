f499daacd09ccf222d0af8345313fba6079efaa8
hegde-atri <iamatrihegde@outlook.com>
2023-01-15 23:33:39 +0000

    fixed changes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 
+const postImportResult = import.meta.glob('./posts/**/*.md*', { eager: true });
+const posts = Object.values(postImportResult);
+
 export const get = () => rss({
   // `<title>` field in output xml
   title: 'Atri’ Posts',
@@ -11,7 +14,12 @@ export const get = () => rss({
   // list of `<item>`s in output xml
   // simple example: generate items for every md file in /src/pages
   // see "Generating items" section for required frontmatter and advanced use cases
-  items: import.meta.glob('./**/*.md'),
+  items: posts.map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.date,
+  })),
   // (optional) inject custom xml
   customData: `<language>en-us</language>`,
+  
 });
