2a46cccb13c97f66ba3feda30995d0b4311ddaf9
hegde-atri <iamatrihegde@outlook.com>
2023-01-16 22:31:53 +0000

    added toc, fixed scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
+import { RehypePlugin } from '@astrojs/markdown-remark';
+import rehypeToc from 'rehype-toc';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new.hegdeatri.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    rehypePlugins: [ rehypeToc],
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'material-palenight',
+      wrap: true,
+    }
+  }
 });
