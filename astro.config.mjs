9ec35869f065668ec6036f96c66f7967c9ffc21b
hegde-atri <iamatrihegde@outlook.com>
2023-01-30 13:27:44 +0000

    updated link

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://new.hegdeatri.com',
+  site: 'https://hegdeatri.com',
   markdown: {
     rehypePlugins: [ rehypeToc ],
     // syntaxHighlight: 'prism',
